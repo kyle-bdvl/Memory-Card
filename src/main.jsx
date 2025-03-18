891aaaa883ebe17d620bab27487e203f3df71659
Kyle <kyle.boudville@gmail.com>
2025-03-18 18:13:11 +0800

    resolved the git issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+//fixed the git init and git remote
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
